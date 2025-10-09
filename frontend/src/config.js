a1c94c2c1ea055c2cc3da4d3ca6ed09e5e8a3716
st182878 <st182878@stud.uni-stuttgart.de>
2025-10-10 00:13:34 +0200

    deploy: drop REST request-id plumbing; Envoy DNS=service names; enable gRPC reflection; stub build fixes; local+docker parity

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE = "http://130.61.225.208:8080"; // OCI Envoy (public)
-export const GRPC_ADDR = "130.61.225.208:8080";
+export const API_BASE = "http://127.0.0.1:8080"; // OCI Envoy (public) 130.61.225.208
+export const GRPC_ADDR = "127.0.0.1:8080";
 export const GRPC_SERVICE = "nexon.grpc.inference.v1.InferenceService/Predict";
